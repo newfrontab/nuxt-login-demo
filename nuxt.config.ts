8d9029e0a41314ec2acc7cf407eae86f25cfec78
Jonathan Hellqvist <newfrontab@gmail.com>
2025-05-13 12:04:11 +0200

    WIP: Working on success page

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  css: ['@/assets/css/global.css'],
+  css: ['@/assets/css/global.scss'],
   vite: {
     vue: {
       template: {
--- pages/Success.vue
+++ pages/Success.vue
@@ -3,14 +3,26 @@
     <provet-card class="container-success">
       <h1 class="n-typescale-2xl header-success">Signup successful 🥳</h1>
 
-      <provet-button class="button-submit" type="button" variant="primary">
-        Login
-      </provet-button>
+      <div class="">
+        <provet-button class="button-submit" type="button" variant="primary">
+          Login
+        </provet-button>
+      </div>
     </provet-card>
+
+    <div class="pyro">
+      <div class="before"></div>
+      <div class="after"></div>
+    </div>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import "@/assets/css/fireworksAnimation.scss";
+</script>
+
+<!-- Import Fireworks animation -->
+
 
 <style scoped lang="scss">
 .wrapper-success {
@@ -22,6 +34,8 @@
 
 .container-success {
   max-width: 400px;
+  z-index: 10;
+  position: relative;
 }
 
 .header-success {
